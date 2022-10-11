b9bbc0c326bad41ac2c563554697d1a22e635208
Ignas <statkus.online@gmail.com>
2022-10-11 21:36:49 +0300

    Task 9

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,25 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive() {
+    return Number(this.budget) > 100000000;
+  }
+}
+
+const matrica = new Movie("The Matrix", "Wachowski Bros.", 63000000);
+const pirates = new Movie(
+  "Pirates of the Caribbean: The Curse of the Black Pearl",
+  "Gore Verbinski",
+  140000000
+);
+
+console.log(matrica.wasExpensive());
+console.log(pirates.wasExpensive());
